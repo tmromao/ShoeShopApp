7f29ceb7c26eebef95175295273e23e6daf059b2
tromao <tiagomromao@gmail.com>
2022-05-04 09:02:27 +0100

    Initial Commit

---
+++ ShoeShopApp/ShoeShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeShopAppApp.swift
+//  ShoeShopApp
+//
+//  Created by Tiago Miguel de Jesus Romao on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoeShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
